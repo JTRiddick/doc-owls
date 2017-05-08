44b8e53c81c466db2440b5709a012fbf23db0f08
JTR <riddick.taylor@gmail.com>
2017-05-08 16:39:25 -0500

    udemy grider tutorial first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,26 @@
-//webpack.config.js
-
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-let extractSCSS = new ExtractTextPlugin({
-  filename:"style.scss",
-  allChunks:true
-})
-let extractCSS = new ExtractTextPlugin('../css/style.css');
-
 module.exports = {
-  entry: path.join(__dirname,'src','app-client.js'),
-  output:{
-    path: path.join(__dirname,'src','static','js'),
-    filename:'bundle.js'
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
   },
-  module:{
-    loaders:[{
-        test: path.join(__dirname,'src'),
-        loader:'babel-loader',
-        query:{
-          presets:['react','es2015']
-        }
-      },
-      {
-        test:/\.scss$/,
-        // loader:ExtractTextPlugin.extract('css-loader!sass-loader')
-        loader: extractCSS.extract('css-loader?modules=true!sass-loader?sourceMap=true')
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
       }
-    ]
+    }]
   },
-  plugins:[
-    extractCSS,
-    extractSCSS,
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings:false},
-      mangle:true,
-      sourcemap:false,
-      beautify:false,
-      dead_code:true
-    }),
-    new ExtractTextPlugin('../css/style.css')
-  ]
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
 };
