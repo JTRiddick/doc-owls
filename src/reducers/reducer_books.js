8fe83ae78f1546970cfe20b8feb0cd17dd5c3541
JTR <riddick.taylor@gmail.com>
2017-05-12 19:18:18 -0500

    initial for weather

--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function(){
-  return [
-    {title: 'Javascript: The Good Parts', pages: 101},
-    {title: 'Harry Potter', pages:390},
-    {title: 'The Dark Tower', pages:285},
-    {title: 'Eloquent Ruby', pages:1}
-  ];
-}
