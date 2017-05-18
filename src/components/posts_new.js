4441dc7b755dce7d5fa54dc75111e6fa10e5a423
JTR <riddick.taylor@gmail.com>
2017-05-18 16:20:44 -0500

    added switch for routes, added redux form

---
+++ src/components/posts_new.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component{
+
+  renderTitleField(field){
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+  render(){
+    return(
+      <form>
+
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+
+      </form>
+
+    );
+  }
+}
+
+//form property must be unique
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
