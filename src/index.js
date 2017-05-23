4d6a66bd9f3c6f51106055d98663dcc99bf8bc62
JTR <riddick.taylor@gmail.com>
2017-05-23 14:12:47 -0500

    connected redux to test API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
 
 import Routes from './routes';
 
+import reducers from './reducers';
+
 import './index.css';
 
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
 ReactDOM.render(
-  <BrowserRouter>
-      <Routes />
-  </BrowserRouter>,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>
+
+  </Provider>,
 
   document.getElementById('root')
 );
